2454bef8f8975bd9908765730f9f9d82de4a011b
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2022-12-06 09:04:34 +0100

    Frontend dla indexu.

--- Restauracja/Services/Restauracja.Services.ProductApi/Data/ApplicationDbContext.cs
+++ Restauracja/Services/Restauracja.Services.ProductApi/Data/ApplicationDbContext.cs
@@ -17,40 +17,44 @@ namespace Restauracja.Services.ProductApi.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Product>().HasData(new Product { 
-                 Id= 1,
-                 Name = "Samosa",
-                 Description= "Description",
-                 CategoryName = "Appetizer",
-                 ImageUrl = ""
-                });
 
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 1,
+                Name = "Samosa",
+                Price = 15,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "",
+                CategoryName = "Appetizer"
+            });
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 Id = 2,
                 Name = "Paneer Tikka",
-                Description = "Description",
-                CategoryName = "Appetizer",
-                ImageUrl = ""
+                Price = 13.99,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "",
+                CategoryName = "Appetizer"
             });
-
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 Id = 3,
                 Name = "Sweet Pie",
-                Description = "Description",
-                CategoryName = "Dessert",
-                ImageUrl = ""
+                Price = 10.99,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "",
+                CategoryName = "Dessert"
             });
-
             modelBuilder.Entity<Product>().HasData(new Product
             {
                 Id = 4,
-                Name = "Bread",
-                Description = "Description",
-                CategoryName = "Entree",
-                ImageUrl = ""
+                Name = "Pav Bhaji",
+                Price = 15,
+                Description = "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "",
+                CategoryName = "Entree"
             });
         }
+
     }
 }
